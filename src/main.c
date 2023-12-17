65240c0e95c96a064047defc26d4aa9080c9109b
Reda96R <redarayyad@yahoo.com>
2023-12-17 15:38:06 +0100

    drawing the map

--- src/main.c
+++ src/main.c
@@ -19,12 +19,14 @@
 #define WIN_X 320
 #define WIN_Y 320
 
+int	g_size = (800 - 5 * 2) / 4;
 int	g_test[][4] = {
 {1, 1, 1, 1},
 {1, 0, 0, 1},
 {1, 0, 0, 1},
 {1, 1, 1, 1}
 };
+
 void	ft_put_pixel(t_mlx *mlx, int x, int y, int color)
 {
 	char	*p;
@@ -36,13 +38,57 @@ void	ft_put_pixel(t_mlx *mlx, int x, int y, int color)
 	}
 }
 
-int	ft_canvas_maker(t_mlx *mlx)
+void	draw_square(t_mlx *mlx, int x, int y, int color)
+{
+	int	w;
+	int	h;
 
+	w = 0;
+	h = 0;
+	while (w < g_size)
+	{
+		h = 0;
+		while (h < g_size)
+		{
+			ft_put_pixel(mlx, x + w, y + h, color);
+			h++;
+		}
+		w++;
+	}
+}
+
+void	ft_draw_map(t_mlx *mlx)
 {
 	int	i;
 	int	j;
+	int	x;
+	int	y;
+	int	color;
 
 	i = 0;
+	y = 0;
+	while (i < 4)
+	{
+		x = 0;
+		j = 0;
+		while (j < 4)
+		{
+			if (g_test[i][j])
+				color = 0xFFFFFF;
+			else
+				color = 0x000000;
+			draw_square(mlx, x, y, color);
+			x += g_size + 2;
+			j++;
+			printf("test\n");
+		}
+		y += g_size + 2;
+		i++;
+	}
+}
+int	ft_canvas_maker(t_mlx *mlx)
+
+{
 	mlx->win_x = 800;
 	mlx->win_y = 800;
 	mlx->mlx_ptr = mlx_init();
@@ -55,7 +101,7 @@ int	ft_canvas_maker(t_mlx *mlx)
 	mlx->img.img = mlx_new_image(mlx->mlx_ptr, mlx->win_x, mlx->win_y);
 	mlx->img.id = mlx_get_data_addr(mlx->img.img, &mlx->img.bpp, &mlx->img.len, \
 	&mlx->img.endian);
-	// while (i <)
+	ft_draw_map(mlx);
 	mlx_put_image_to_window(mlx->mlx_ptr, mlx->win_ptr, mlx->img.img, 0, 0);
 	mlx_loop(mlx->mlx_ptr);
 	return (1);
